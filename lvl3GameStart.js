929bc48e493b481327b56109344095354c9a51fd
Victor <nastoyashchiy@gmail.com>
2023-07-19 14:32:25 +0300

    finished14.0

--- lvl3GameStart.js
+++ lvl3GameStart.js
@@ -6,14 +6,14 @@ class lvl3GameStart extends Phaser.Scene {
 	preload() {
 		this.load.video('opening3', './media/videos/opening-lvl3.mp4');
 		this.load.audio('backgroundMusic3', './media/sounds/theme-lvl3.mp3');    		
-	}
+	} 
 
 	create() {
 		this.backgroundMusic = this.sound.add('backgroundMusic3', { loop: true });    
     this.backgroundMusic.play();   
    
     const screenWidth = this.scale.width / 2;
-    const videoSprite = this.add.video(screenWidth, 300, 'opening3').setScale(0.7);
+    const videoSprite = this.add.video(screenWidth, 300, 'opening3').setScale(0.25);
     videoSprite.play(true);
     videoSprite.setLoop(false);
 
